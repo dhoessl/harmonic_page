3f076ee3fd841e7ac52016bd1e5e420d7228725a
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-14 20:46:33 +0200

    fix go (possibly)

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,6 +3,8 @@
 git clone https://github.com/dhoessl/harmonic_page.git
 cd harmonic_page/files/harmonic
 
+export PATH=$PATH:/go/bin
+
 hugo mod init harmonic
 hugo mod get -u
 hugo --destination /public
